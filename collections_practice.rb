679570e3227ef3b128308e4e37e1541335de8623
Alma Flores <flores.almalee@gmail.com>
2020-08-19 03:34:01 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -49,11 +49,5 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect {|element, index| index != 1 ? 
-    if index == 1 
-    
-    else
-      element + 's'
-    end
-  end
+  array.each_with_index.collect {|element, index| index != 1 ? element + 's' : }
 end
