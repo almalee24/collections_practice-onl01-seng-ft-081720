2ba5be308320d74051f185af8faf8511a9a12df3
Alma Flores <flores.almalee@gmail.com>
2020-08-19 03:33:13 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -49,7 +49,7 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect {|element, index| 
+  array.each_with_index.collect {|element, index| index != 1 ? 
     if index == 1 
     
     else
